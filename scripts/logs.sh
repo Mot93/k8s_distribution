5dcf295ba4ef4d2745455a781c22dcf763c490ac
Mattia Rubini <Mattia.rubini@gmail.com>
2026-02-22 12:25:20 +0100

    add: logs script

---
+++ scripts/logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source=./config.env
+
+# Log function
+log() {
+    # Get level, message and file where to store
+    local level=$1
+    local message=$2
+    local log_file="logs/example.log"
+    # Config logs
+    local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+    local log_entry="[$timestamp] [$level] $message"
+
+    # Color config
+    # Define colors
+    local RED='\033[0;31m'
+    local YELLOW='\033[1;33m'
+    local GREEN='\033[0;32m'
+    local NC='\033[0m' # No Color
+
+    # Print to console with color
+    case $level in
+        "INFO")
+            echo -e "${GREEN}$log_entry${NC}"
+            ;;
+        "WARNING")
+            echo -e "${YELLOW}$log_entry${NC}"
+            ;;
+        "ERROR")
+            echo -e "${RED}$log_entry${NC}"
+            ;;
+        *)
+            echo -e "$log_entry"
+            ;;
+    esac
+
+    # Create directories if they don't exist
+    mkdir -p "$(dirname "$log_file")"
+
+    # Create the log file if it doesn't exist
+    touch "$log_file"
+
+    # Print to log file (no color)
+    echo "$log_entry" >> "$log_file"
+}
+
+# Example usage
+#log "INFO" "Script started" "logfile.txt"
+
+export -f log
