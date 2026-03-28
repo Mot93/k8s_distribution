093c7f83192385b4e381fdbdb98001c9312a551f
Mattia Rubini <Mattia.rubini@gmail.com>
2026-03-28 16:13:35 +0100

    update: stricter shell setup, added logging into file and fixed export configurations

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# set -eu # Exit immediately if any command fails or if an unset variable is used
-set -e # Exit immediately if any command fails
+set -euo pipefail # Exit immediately if any command fails, if an unset variable is used or if any command in the pipeline fails
 
 # Logs passed level and message
 # A third argument can be passed as the path to the file where to store logs 
@@ -46,14 +45,13 @@ log() {
     esac
 
     # Record the log into a file only if a third argument containing the path to the log has been passed
-    # if [ ! $# -ge 3 ]; then
-    #     local log_file=$3
-    #     echo $log_file
-    #     # Create directories if they don't exists
-    #     mkdir -p "$(dirname "$log_file")"
-    #     # Print to log to the specified file (no color)
-    #     echo "$log_entry" >> "$log_file"
-    # fi
+    if [ $# -ge 3 ]; then
+        local log_file=$3
+        # Create directories if they don't exists
+        mkdir -p "$(dirname "$log_file")"
+        # Print to log to the specified file (no color)
+        echo "$log_entry" >> "$log_file"
+    fi
 }
 
 log_file_name() {
@@ -67,8 +65,6 @@ log_file_name() {
     echo "${file_name}_${timestamp}.log"
 }
 
-export -f log log_file_name
-
 test() {
     # Config
     log_file=$(log_file_name "test")
@@ -79,3 +75,5 @@ test() {
     log "SUCCESS" "Youy made it 😎"
     log "CUSTOM" "We are going off road!! 🚧"
 }
+
+export log log_file_name test
