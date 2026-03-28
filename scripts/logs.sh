1c8d4f42365f0e1f625da294fe01ec4ed3367f0c
Mattia Rubini <Mattia.rubini@gmail.com>
2026-03-28 13:03:45 +0100

    update: support success and added function helper for the creation of log file name

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-# Example usage
-# log "INFO" "Script started" "log_file.log"
-
-# Log function
+# Logs passed level and message
+# A third argument can be passed as the path to the file where to store logs 
 log() {
-    if [ ! $# -ge 3 ]; then
-        echo "ERROR: 3 argument are required: level message log_file"
+    if [ ! $# -ge 2 ]; then
+        echo "ERROR: 2 argument are required: <level> <message>"
         exit 1
     fi
     # Get level, message and file where to store
     local level=$1
     local message=$2
-    local log_file=$3
     # Config logs
     local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
     local log_entry="[$timestamp] [$level] $message"
 
     # Color config
     # Define colors
+    local LIGHT_BLUE='\033[0;34m'
+    local YELLOW='\033[0;33m'
     local RED='\033[0;31m'
-    local YELLOW='\033[1;33m'
     local GREEN='\033[0;32m'
+    local LIGHT_PURPLE='\033[0;35m'
     local NC='\033[0m' # No Color
 
     # Print to console with color
     case $level in
         "INFO")
-            echo -e "${GREEN}$log_entry${NC}"
+            echo -e "${LIGHT_BLUE}$log_entry${NC}"
             ;;
         "WARNING")
             echo -e "${YELLOW}$log_entry${NC}"
@@ -35,19 +34,34 @@ log() {
         "ERROR")
             echo -e "${RED}$log_entry${NC}"
             ;;
+        "SUCCESS")
+            echo -e "${GREEN}$log_entry${NC}"
+            ;;
         *)
-            echo -e "$log_entry"
+            echo -e "${LIGHT_PURPLE}$log_entry${NC}"
             ;;
     esac
 
-    # Create directories if they don't exist
-    mkdir -p "$(dirname "$log_file")"
-
-    # Create the log file if it doesn't exist
-    touch "$log_file"
-
-    # Print to log file (no color)
-    echo "$log_entry" >> "$log_file"
+    # Record the log into a file only if a third argument containing the path to the log has been passed
+    # if [ ! $# -ge 3 ]; then
+    #     local log_file=$3
+    #     echo $log_file
+    #     # Create directories if they don't exists
+    #     mkdir -p "$(dirname "$log_file")"
+    #     # Print to log to the specified file (no color)
+    #     echo "$log_entry" >> "$log_file"
+    # fi
 }
 
-export -f log
+log_file_name() {
+    if [ ! $# -ge 1 ]; then
+        echo "ERROR: 1 argument is required: <name>"
+        exit 1
+    else
+        file_name="$1"
+    fi
+    timestamp=$(date +"%Y-%m-%d")
+    echo "${file_name}_${timestamp}.log"
+}
+
+export -f log log_file_name
